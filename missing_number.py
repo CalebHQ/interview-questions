c90a7ab6647cd468a6883c7750dc362919c9bc3c
Caleb McLeod <caleb.mcleod0@gmail.com>
2021-01-08 14:44:18 +1100

    find missing number in an array

---
+++ missing_number.py
@@ -0,0 +1,15 @@
+# FIND MISSING NUMBER IN AN ARRAY
+
+def missing(list):
+    last_num = list[-1]
+    natural_sum = (last_num * (last_num +1 )) // 2
+    
+    sum = 0
+    for i in list:
+        sum += i
+
+    missing_number = natural_sum - sum
+    return missing_number
+
+
+print(missing([1, 2, 3, 4, 6, 7, 8, 9, 10]))
